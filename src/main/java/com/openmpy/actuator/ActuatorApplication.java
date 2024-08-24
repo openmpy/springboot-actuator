2beb5c9677e7a8684e2fe8cf46beb5b561352d41
openmpy <suhwan@kakao.com>
2024-08-24 23:10:00 +0900

    feat: Add initial setup for Spring Boot actuator project

---
+++ src/main/java/com/openmpy/actuator/ActuatorApplication.java
@@ -0,0 +1,13 @@
+package com.openmpy.actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActuatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/openmpy/actuator/ActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openmpy.actuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActuatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
