b70494dcdb70cce6a5285160e1a0c20f94d8d784
openmpy <suhwan@kakao.com>
2024-08-26 08:56:38 +0900

    feat: Add OrderServiceV1 with metrics and update configuration

---
+++ src/main/java/com/openmpy/actuator/order/v1/OrderServiceV1.java
@@ -0,0 +1,50 @@
+package com.openmpy.actuator.order.v1;
+
+import com.openmpy.actuator.order.OrderService;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Slf4j
+public class OrderServiceV1 implements OrderService {
+
+    private final AtomicInteger stock = new AtomicInteger(100);
+    private final MeterRegistry registry;
+
+    public OrderServiceV1(MeterRegistry registry) {
+        this.registry = registry;
+    }
+
+    @Override
+    public void order() {
+        log.info("주문");
+        stock.decrementAndGet();
+
+        Counter.builder("my.order")
+                .tag("class", this.getClass().getName())
+                .tag("method", "order")
+                .description("order")
+                .register(registry)
+                .increment();
+    }
+
+    @Override
+    public void cancel() {
+        log.info("취소");
+        stock.incrementAndGet();
+
+        Counter.builder("my.order")
+                .tag("class", this.getClass().getName())
+                .tag("method", "cancel")
+                .description("order")
+                .register(registry)
+                .increment();
+    }
+
+    @Override
+    public AtomicInteger getStock() {
+        return stock;
+    }
+}
