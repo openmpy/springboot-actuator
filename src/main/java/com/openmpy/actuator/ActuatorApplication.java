70bd44d71f0e4a0513ef42ae09e5ebf275e3fd5a
openmpy <suhwan@kakao.com>
2024-08-26 09:00:20 +0900

    feat: Add OrderServiceV2 implementation with metrics support

--- src/main/java/com/openmpy/actuator/ActuatorApplication.java
+++ src/main/java/com/openmpy/actuator/ActuatorApplication.java
@@ -1,13 +1,13 @@
 package com.openmpy.actuator;
 
-import com.openmpy.actuator.order.v1.OrderConfigV1;
+import com.openmpy.actuator.order.v2.OrderConfigV2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
-@Import(OrderConfigV1.class)
+@Import(OrderConfigV2.class)
 @SpringBootApplication(scanBasePackages = "com.openmpy.actuator.controller")
 public class ActuatorApplication {
 
